83d4cd898cbbb145cbd7f3753b33514ba061cd2f
Michael McCourt <mccourt@sigopt.com>
2021-01-06 20:43:55 -0500

    Don't require quite so much

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lookahead',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
-    # install_requires=['scikit-learn', 'scipy', 'numpy', 'torch', 'gpytorch']
-    install_requires=['scikit-learn', 'scipy', 'numpy', 'jaxlib', 'numpyro', 'torch', 'qmcpy']
+    install_requires=['scikit-learn', 'scipy', 'numpy', 'qmcpy']
 )
